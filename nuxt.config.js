3209a1fbfdf67a080dd77517a6ab91e76fb7b62e
liuguojun <gj.liu@sj-sol.com>
2021-07-21 05:41:29 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,22 @@
+/**
+ * Nuxt.js 配置文件
+ */
+
+module.exports = {
+    router: {
+        // 自定义路由表规则
+        // routes: 一个数组，路由配置表
+        // resolve: 解析路由组件路径
+        extendRoutes(routes, resolve) {
+            // 清除 Nuxt.js 基于 pages 目录默认生成的路由表规则
+            routes.splice(0)
+            // 添加自己的路由规则
+            routes.push(...[
+                {
+                    path: '/',
+                    component: resolve(__dirname, 'pages/layout/')
+                }
+            ])
+        }
+    }
+}
