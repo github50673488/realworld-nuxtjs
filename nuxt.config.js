11415c5e34b030eba19c4aa32a6640e8169635f8
liuguojun <gj.liu@sj-sol.com>
2021-07-24 13:13:42 +0900

    add all other page

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,6 +58,10 @@ module.exports = {
         }
     },
 
+    server: {
+        host: '0.0.0.0', // 默认localhost
+        port: 3000
+    },
     // 注册插件
     plugins: [
         '~/plugins/request.js',
