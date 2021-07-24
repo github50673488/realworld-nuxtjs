1dbfb58bffc1b4987d0b7ff46b92af9ed9936c6b
liuguojun <gj.liu@sj-sol.com>
2021-07-24 11:54:35 +0900

    展示文章作者相关信息

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -23,25 +23,7 @@
       <hr/>
 
       <div class="article-actions">
-        <div class="article-meta">
-          <a href="profile.html"><img src="http://i.imgur.com/Qr71crq.jpg"/></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp;
-            Follow Eric Simons <span class="counter">(10)</span>
-          </button>
-          &nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp;
-            Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
+        <article-meta :article="article"/>
       </div>
 
       <div class="row">
@@ -106,6 +88,7 @@
 
 import {getArticle} from "../../api/article";
 import MarkdownIt from 'markdown-it'
+import ArticleMeta from './components/article-meta.vue'
 
 export default {
   name: 'ArticleIndex',
@@ -120,6 +103,11 @@ export default {
       article: article
     }
   }
+  ,
+  components: {
+    ArticleMeta,
+
+  },
 }
 </script>
 
